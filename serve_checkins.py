1309078d1d058d298940069c4df10c7396578ceb
Chadwick <chadwick.employment@gmail.com>
2025-01-05 19:00:50 +0100

    checks on serving and finding the right paths

--- serve_checkins.py
+++ serve_checkins.py
@@ -22,4 +22,16 @@ async def get0():
 @app.get("/next")
 async def fakedb(skip: int = 0, limit: int = 10):
     """ http "127.0.0.1:8000/next?skip=20&limit=5" """
+    error = []
+    checkins_last = len(checkins) - 1
+    if skip < 0 or skip > checkins_last:
+        error.append(f"skip too great or small: {skip}")
+    if limit < 0:
+        error.append(f"limit {limit}: less than 0")
+    if skip + limit > checkins_last:
+        limit = checkins_last
+
+    if error:
+        return {"error": " ".join(error)}
+
     return checkins[skip: skip + limit]
--- work.py
+++ work.py
@@ -2,10 +2,9 @@ from pathlib import Path
 import json
 
 
-db = Path("db/checkins.json")
-with open(db, "r") as dbh:
-    checkins = json.load(dbh)
+checkins = json.loads(Path("db/checkins.json").read_text())
 
+checkin_0_0 = checkins[0]['response']['checkins']['items'][0]
 print(f"checkins type: {type(checkins)}")
 print(f"checkins len: {len(checkins)}")
 print(f"checkins[0] type: {type(checkins[0])}")
@@ -13,14 +12,19 @@ print(f"checkins[0] keys: {checkins[0].keys()}")
 print("\ncheckins[0] values types: \n"
       f"{[type(v) for v in checkins[0].values()]}\n"
       )
-print(f"checkins[0] beer: {checkins[0]['beer']}")
-print(f"checkins[0] beer name: {checkins[0]['beer']['beer_name']}")
-print(f"checkins[0] beer abv: {checkins[0]['beer']['beer_abv']}")
-print(f"checkins[0] checkin_id: {checkins[0]['checkin_id']}")
+print(f"checkins[0] beer: {checkin_0_0['beer']}")
+print(f"checkins[0] beer name: {checkin_0_0['beer']['beer_name']}")
+print(f"checkins[0] beer abv: {checkin_0_0['beer']['beer_abv']}")
+print(f"checkins[0] checkin_id: {checkin_0_0['checkin_id']}")
 
-checks = {checkin["checkin_id"]: checkin["created_at"] for checkin in checkins}
+checks = {
+    checkin["checkin_id"]: checkin["created_at"]
+    for checkin_list in checkins
+    for checkin in checkin_list["response"]["checkins"]["items"]
+}
 bierlist = [
     (checkin["checkin_id"], checkin["created_at"])
-    for checkin in checkins
+    for checkin_list in checkins
+    for checkin in checkin_list["response"]["checkins"]["items"]
 ]
 print(f"eerst: {bierlist[0]}\nlaatst: {bierlist[-1]}")
